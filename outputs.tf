22528a1347c6daeb8854eb8236f5276f529cd080
Kevin Stenerson <kstenerson@auditboard.com>
2020-03-06 11:45:55 -0800

    Add a description to the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "vpc_id" {
   value = aws_vpc.default.id
+  description ="the AWS ID of the VPC"
 }
 
 output "private_subnet_ids" {
   value = [aws_subnet.private.*.id]
+  description ="the AWS IDs of all private subnets of the VPC"
 }
 
 output "public_subnet_ids" {
   value = [aws_subnet.public.*.id]
+  description ="the AWS IDs of all public subnets of the VPC"
 }
 
 output "cidr_block" {
   value = aws_vpc.default.cidr_block
+  description = "the full cidr block notation for the VPC"
 }
